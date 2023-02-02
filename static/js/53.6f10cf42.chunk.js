c14cdf96ae237327b035005decf4ad83dd421933
NikitaSokolovUA <107192011+NikitaSokolovUA@users.noreply.github.com>
2023-02-02 17:53:45 +0000

    Deploying to gh-pages from @ NikitaSokolovUA/Phonebook@47e777875b97d9d4330f8da03cfc1d429425c3e8 🚀

--- static/js/53.6f10cf42.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[53],{6053:function(t,e,r){r.d(e,{v8y:function(){return n}});var a=r(9983);function n(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",stroke:"#000",strokeWidth:"2",d:"M7,7 L17,17 M7,17 L17,7"}}]})(t)}}}]);
-//# sourceMappingURL=53.6f10cf42.chunk.js.map
